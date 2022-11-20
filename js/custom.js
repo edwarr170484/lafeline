899f6c208c57f9749bbffd63efd85f922e76a66e
Эдгар Жизневский <edwarr170484@gmail.com>
2022-11-21 00:58:19 +0300

    Add modal layout

--- js/custom.js
+++ js/custom.js
@@ -14,3 +14,14 @@ function closeMenu(event) {
     let menu = document.getElementsByClassName('header__dropdown-menu')[0];
     menu.classList.remove('opened');
 }
+
+$('.form__select').on('click', function () {
+    $(this).toggleClass('form__select_opened');
+});
+
+$('.form__select-option').on('click', function (event) {
+    let value = $(this).data('option');
+    let label = $(this).find('span').text();
+    $(this).parent().next('input[type="hidden"]').val(value);
+    $(this).parent().parent().find('.form__field-label').addClass('form__field-label_selected').text(label);
+});
